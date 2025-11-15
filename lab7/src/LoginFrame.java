b8710d201bdc6ab9a6c7f2b46aa8d65954cc4d71
danah19112005-lang <danah19112005@gmail.com>
2025-11-15 21:34:18 +0200

    start LoginFrame Class

--- lab7/src/LoginFrame.java
+++ lab7/src/LoginFrame.java
@@ -2,11 +2,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 /**
  *
  * @author DANAH
  */
-public class LoginFrame {
-    
-}
+public class LoginFrame extends JFrame {
+
+    private JTextField emailField;
+    private JPasswordField passwordField;
+    private JButton loginBtn, signupBtn;
+
+    public LoginFrame() {
+        setTitle("Login");
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(4, 1));
+
+        

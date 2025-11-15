df131e4740950a310eff0166cdc5b6dda5b59774
danah19112005-lang <danah19112005@gmail.com>
2025-11-15 21:39:25 +0200

    Update LoginFrame.java

--- lab7/src/LoginFrame.java
+++ lab7/src/LoginFrame.java
@@ -22,4 +22,47 @@ public class LoginFrame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(4, 1));
 
+        JPanel emailPanel = new JPanel();
+        emailPanel.add(new JLabel("Email: "));
+        emailField = new JTextField(20);
+        emailPanel.add(emailField);
+        add(emailPanel);
         
+        JPanel btnPanel = new JPanel();
+        loginBtn = new JButton("Login");
+        signupBtn = new JButton("Go to Signup");
+        btnPanel.add(loginBtn);
+        btnPanel.add(signupBtn);
+        add(btnPanel);
+        
+        loginBtn.addActionListener(e -> loginAction());
+        signupBtn.addActionListener(e -> {
+            dispose();
+            new SignupFrame().setVisible(true);
+        });
+    }
+
+    private void loginAction() {
+        String email = emailField.getText().trim();
+        String password = new String(passwordField.getPassword()).trim();
+        
+         if (email.isEmpty() || password.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Please fill all fields.");
+            return;
+        }
+
+        if (!email.contains("@") || !email.contains(".")) {
+            JOptionPane.showMessageDialog(this, "Invalid email format.");
+            return;
+        }
+        
+        String role = email.startsWith("i") ? "instructor" : "student";
+        
+        dispose();
+        if (role.equals("student")) {
+            new StudentDashboardFrame().setVisible(true);
+        } else {
+            new InstructorDashboardFrame().setVisible(true);
+        }
+    }
+}

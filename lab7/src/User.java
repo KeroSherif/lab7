f951bf383c12058bcd78ae76ebccd6246545fa96
monicaazer50-cyber <monicaazer50@gmail.com>
2025-11-16 00:56:19 +0200

    ش

--- lab7/src/User.java
+++ lab7/src/User.java
@@ -7,15 +7,69 @@
  *
  * @author monic
  */
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-public class User {
-    public int userId;
-    public String role;
-    public String username;
-    public String email;
-    public String passwordHash;
-    public List<Integer> createdCourses;
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "role")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = Student.class, name = "student"),
+    @JsonSubTypes.Type(value = Instructor.class, name = "instructor")
+})
+public abstract class User { // abstract class
+    protected String userId;
+    protected String role; // "student" أو "instructor"
+    protected String username;
+    protected String email;
+    protected String passwordHash; // SHA-256 hash
 
-    public User() {}
+    public User() {} // Constructor فاضي لـ Jackson
+
+    public User(String userId, String role, String username, String email, String passwordHash) {
+        this.userId = userId;
+        this.role = role;
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    // Getters and Setters
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
 }

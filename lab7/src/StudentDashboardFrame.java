f0c95421c0fc20616731157fa15eae62729baa4a
danah19112005-lang <danah19112005@gmail.com>
2025-11-15 21:49:27 +0200

    Create StudentDashboardFrame.java

---
+++ lab7/src/StudentDashboardFrame.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import javax.swing.*;
+import java.awt.*;
+/**
+ *
+ * @author DANAH
+ */
+public class StudentDashboardFrame extends JFrame {
+
+    public StudentDashboardFrame() {
+        setTitle("Student Dashboard");
+        setSize(500, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(4, 1));

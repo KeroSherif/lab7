2060e2fbaa74c3c2d3df0f851edfc0b5a3b3ea74
danah19112005-lang <danah19112005@gmail.com>
2025-11-15 21:44:22 +0200

    Update SignupFrame.java

--- lab7/src/SignupFrame.java
+++ lab7/src/SignupFrame.java
@@ -21,3 +21,33 @@ public class SignupFrame extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(6, 1));
+
+        JPanel p1 = new JPanel();
+        p1.add(new JLabel("Username: "));
+        usernameField = new JTextField(20);
+        p1.add(usernameField);
+        add(p1);
+        
+        JPanel p2 = new JPanel();
+        p2.add(new JLabel("Email: "));
+        emailField = new JTextField(20);
+        p2.add(emailField);
+        add(p2);
+        
+        JPanel p3 = new JPanel();
+        p3.add(new JLabel("Password: "));
+        passwordField = new JPasswordField(20);
+        p3.add(passwordField);
+        add(p3);   
+
+        JPanel p4 = new JPanel();
+        p4.add(new JLabel("Confirm Password: "));
+        confirmPasswordField = new JPasswordField(20);
+        p4.add(confirmPasswordField);
+        add(p4);
+        
+        JPanel p5 = new JPanel();
+        p5.add(new JLabel("Role: "));
+        roleBox = new JComboBox<>(new String[]{"Student", "Instructor"});
+        p5.add(roleBox);
+        add(p5);

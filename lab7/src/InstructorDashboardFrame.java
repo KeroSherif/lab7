c40719f6d2b8b4ffc7917a88f2c923739027ef4e
danah19112005-lang <danah19112005@gmail.com>
2025-11-15 21:52:26 +0200

    Create InstructorDashboardFrame.java

---
+++ lab7/src/InstructorDashboardFrame.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import javax.swing.*;
+import java.awt.*;
+/**
+ *
+ * @author DANAH
+ */
+public class InstructorDashboardFrame extends JFrame {
+
+    public InstructorDashboardFrame() {
+        setTitle("Instructor Dashboard");
+        setSize(500, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(5, 1));

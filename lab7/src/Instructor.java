bf6344ebf34ecf6068e202fdd004581e74a1eb45
monicaazer50-cyber <monicaazer50@gmail.com>
2025-11-16 01:21:39 +0200

    .

--- lab7/src/Instructor.java
+++ lab7/src/Instructor.java
@@ -7,6 +7,30 @@
  *
  * @author monic
  */
-public class Instructor {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class Instructor extends User {
+
+    private List<Integer> createdCourses = new ArrayList<>();
+
+    public Instructor() {
+        super(); // Constructor فارغ لـ Jackson
+    }
+
+    public Instructor(String userId, String username, String email, String passwordHash) {
+        super(userId, "instructor", username, email, passwordHash);
+        this.createdCourses = new ArrayList<>();
+    }
+
+    // Getter & Setter
+    public List<Integer> getCreatedCourses() {
+        return createdCourses;
+    }
+
+    public void setCreatedCourses(List<Integer> createdCourses) {
+        this.createdCourses = createdCourses;
+    }
+
+    // يمكن إضافة وظائف خاصة بالـ Instructor لاحقاً مثل: حذف كورس، تعديل كورس، ...
 }

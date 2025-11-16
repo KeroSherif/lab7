27df8e7afefbf4d681d84dbc880f841de31188a8
KeroSherif <kerosherif8@gmail.com>
2025-11-16 13:16:40 +0200

    Update StudentService.java

--- lab7/src/StudentService.java
+++ lab7/src/StudentService.java
@@ -2,8 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package lab7;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;

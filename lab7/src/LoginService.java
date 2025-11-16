f1d3a628af27e684c7844311839371962351930a
mohamed alzagh <164375339+nuggut@users.noreply.github.com>
2025-11-17 00:32:36 +0200

    added validation in LoginService

--- lab7/src/LoginService.java
+++ lab7/src/LoginService.java
@@ -14,18 +14,36 @@ public class LoginService {
     }
 
     public User login(String email, String plainPassword) throws Exception{
+        
+        String validationError = Validation.validateLogin(email, plainPassword);
+        if (!validationError.isEmpty()) {
+            throw new Exception(validationError);
+        }
+
         User user = dbManager.getUserByEmail(email);
         if(user == null){
             throw new Exception("This Email is not found");
         }
         String enteredPasswordEncrypted = PasswordEncryption.hashPassword(plainPassword);
-        if(!enteredPasswordEncrypted.equals(user.getPasswordHash())){ // صححت الاسم من getpasswordHash لـ getPasswordHash
+        if(!enteredPasswordEncrypted.equals(user.getPasswordHash())){
             throw new Exception("Wrong password");
         }
         return user;
     }
-
     public User signup(String username, String email, String plainPassword, String role) throws Exception{
+        String vUsername = Validation.validateUsername(username);
+        if (!vUsername.isEmpty()) throw new Exception(vUsername);
+
+        String vEmail = Validation.validateEmail(email);
+        if (!vEmail.isEmpty()) throw new Exception(vEmail);
+
+        
+        String vPassword = Validation.validatePassword(plainPassword);
+        if (!vPassword.isEmpty()) throw new Exception(vPassword);
+
+        String vRole = Validation.validateRole(role);
+        if (!vRole.isEmpty()) throw new Exception(vRole);
+        
         if(dbManager.getUserByEmail(email) != null){
             throw new Exception("This email is unavailable");
         }
@@ -33,7 +51,7 @@ public class LoginService {
         String userId = dbManager.getNextUserId();
         User newUser;
         if(role.equalsIgnoreCase("student")){
-            newUser = new Student(userId, username, email, hashedPassword); // صححت الاسم من student لـ Student
+            newUser = new Student(userId, username, email, hashedPassword);
         }else{
             newUser = new Instructor(userId, username, email, hashedPassword);
         }

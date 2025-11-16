c9703456c1a90036c60218e908096c39d134d9f9
mohamed alzagh <164375339+nuggut@users.noreply.github.com>
2025-11-16 10:57:58 +0200

    Fixed errors

--- LoginService.java
+++ LoginService.java
@@ -1,9 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package frontend.lab7;
-
 /**
  *
  * @author Mohamed
@@ -40,3 +34,4 @@ public class LoginService {
         return newUser;
     }
 }
+

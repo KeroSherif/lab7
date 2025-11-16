3f2549d4449e6e4354459d504f4f6214665e6f98
KeroSherif <kerosherif8@gmail.com>
2025-11-16 12:54:51 +0200

    Create Student.java

---
+++ lab7/src/Student.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab7;
+
+import java.util.*;
+
+/**
+ *
+ * @author Kirolos sherif
+ */
+public class Student extends User {
+
+    private List<String> enrolledCourses; 
+    private Map<String, List<String>> progress; 
+    // progress: key = courseId, value = list of completed lessonIds
+
+    public Student(String userId, String username, String email, String passwordHash) {
+        super(userId, "student", username, email, passwordHash);
+        this.enrolledCourses = new ArrayList<>();
+        this.progress = new HashMap<>();
+    }
+
+    public List<String> getEnrolledCourses() {
+        return enrolledCourses;
+    }
+
+    public Map<String, List<String>> getProgress() {
+        return progress;
+    }
+
+    public void enrollCourse(String courseId) {
+        if (!enrolledCourses.contains(courseId)) {
+            enrolledCourses.add(courseId);
+            progress.put(courseId, new ArrayList<>());
+        }
+    }
+
+    public void markLessonCompleted(String courseId, String lessonId) {
+        if (!progress.containsKey(courseId))
+            progress.put(courseId, new ArrayList<>());
+
+        List<String> completed = progress.get(courseId);
+        if (!completed.contains(lessonId)) {
+            completed.add(lessonId);
+        }
+    }
+}
+

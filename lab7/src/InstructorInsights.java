d5669fe849c219e6512289c1adf586bc0d00f37a
danah19112005-lang <danah19112005@gmail.com>
2025-11-23 00:37:31 +0200

    Create InstructorInsights.java

---
+++ lab7/src/InstructorInsights.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DANAH
+ */
+public class InstructorInsights {
+    public int totalCourses;
+    public int totalStudents;
+    public double averageQuiz;
+
+    public InstructorInsights(int totalCourses, int totalStudents, double averageQuiz) {
+        this.totalCourses = totalCourses;
+        this.totalStudents = totalStudents;
+        this.averageQuiz = averageQuiz;
+    }
+}

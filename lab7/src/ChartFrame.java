9a13545370dce1df4ccb2cd4ba59bf0e1551551e
danah19112005-lang <danah19112005@gmail.com>
2025-11-22 23:29:38 +0200

    Create ChartFrame.java

---
+++ lab7/src/ChartFrame.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import javax.swing.*;
+
+/**
+ *
+ * @author DANAH
+ */
+public class ChartFrame extends JFrame {
+    public ChartFrame(String title, JFreeChart chart) {
+        setTitle(title);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        add(new ChartPanel(chart));
+    }
+}
+

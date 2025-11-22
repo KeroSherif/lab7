ce164e0179303fd8d699d086a191d8dc33a826ff
danah19112005-lang <danah19112005@gmail.com>
2025-11-22 22:55:55 +0200

    Update QuizResult.java

--- lab7/src/QuizResult.java
+++ lab7/src/QuizResult.java
@@ -8,5 +8,20 @@
  * @author DANAH
  */
 public class QuizResult {
-    
+    private String studentId;
+    private String courseId;
+    private String lessonId;
+    private int score;
+
+    public QuizResult(String studentId, String courseId, String lessonId, int score) {
+        this.studentId = studentId;
+        this.courseId = courseId;
+        this.lessonId = lessonId;
+        this.score = score;
+    }
+
+    public String getStudentId() { return studentId; }
+    public String getCourseId() { return courseId; }
+    public String getLessonId() { return lessonId; }
+    public int getScore() { return score; }
 }

51f4cba61591c0a78d5569129d9a84713d9d831c
KeroSherif <kerosherif8@gmail.com>
2025-11-23 17:33:12 +0200

    edit

--- lab7/src/ChartFrame.java
+++ lab7/src/ChartFrame.java
@@ -1,20 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 import javax.swing.*;
-import org.jfree.chart.*;
-import org.jfree.chart.panel.ChartPanel;
-/**
- *
- * @author DANAH
- */
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.ChartPanel;
+
 public class ChartFrame extends JFrame {
+
     public ChartFrame(String title, JFreeChart chart) {
-        setTitle(title);
+        super(title);
+
+       
         setSize(800, 600);
         setLocationRelativeTo(null);
-        add(new ChartPanel(chart));
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        
+        ChartPanel chartPanel = new ChartPanel(chart);
+        setContentPane(chartPanel);
     }
 }
-
